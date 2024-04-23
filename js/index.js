b1f6798756f92a3ca7877ea51affcb17e4967c06
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2024-04-23 05:46:19 -0400

    Created basic project container

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
+const navItems = document.getElementById("navLinks");
+const navLinks = document.getElementsByClassName("nav-link-item");
+
+navItems.addEventListener("transitionend", function (event) {
+    if (navItems.classList.contains("hidden")) {
+        navItems.style.zIndex = -1;
+    }
+    navItems.style.zIndex = 0;
+});
+
+navItems.addEventListener("transitionstart", function (event) {
+    navItems.style.zIndex = -1;
+});
+
+Array.prototype.forEach.call(navLinks, function (event) {
+    event.addEventListener("click", function (e) {
+        console.log("click!");
+        navItems.classList.toggle("show");
+        navItems.classList.toggle("hidden");
+    });
+});
+
 function toggleMobileMenu() {
-    const navItems = document.getElementById("navLinks");
     navItems.classList.toggle("show");
     navItems.classList.toggle("hidden");
 }

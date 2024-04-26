c4dca63dc4c156ae8004013976482bde8b92f92b
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2024-04-26 06:41:34 -0400

    Added theme switcher

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const navItems = document.getElementById("navLinks");
 const navLinks = document.getElementsByClassName("nav-link-item");
 const menuBtn = document.getElementById("menuButton");
 const closeBtn = document.getElementById("closeButton");
+const lightBtn = document.getElementById("lightMode");
+const darkBtn = document.getElementById("darkMode");
+
+let theme = "dark";
 
 navItems.addEventListener("transitionstart", function (event) {
     if (event.propertyName === "top") {
@@ -33,4 +37,41 @@ navItems.addEventListener("transitionstart", function (event) {
 function toggleMobileMenu() {
     navItems.classList.toggle("show");
     navItems.classList.toggle("hidden");
+}
+
+function changeTheme() {
+    lightBtn.classList.toggle("hide");
+    darkBtn.classList.toggle("hide");
+    if (theme === "dark") {
+        // Change to light
+        theme = "light";
+        document.body.style.setProperty("--bg-color", "#fff");
+        document.body.style.setProperty("--secondary-color", "#bbbbbb");
+        document.body.style.setProperty("--text-color", "#000");
+    } else {
+        // Change to dark
+        theme = "dark";
+        document.body.style.setProperty("--bg-color", "#444");
+        document.body.style.setProperty("--secondary-color", "#333");
+        document.body.style.setProperty("--text-color", "#fff");
+    }
+}
+
+if (window.innerWidth <= 768) {
+    closeBtn.classList.add("hide");
+}
+
+if (theme === "dark") {
+    document.body.style.setProperty("--bg-color", "#444");
+    document.body.style.setProperty("--secondary-color", "#333");
+    document.body.style.setProperty("--text-color", "#fff");
+} else if (theme === "light") {
+    document.body.style.setProperty("--bg-color", "#fff");
+    document.body.style.setProperty("--secondary-color", "#bbbbbb");
+    document.body.style.setProperty("--text-color", "#000");
+} else {
+    console.log("Error occured. Defaulting to dark theme.");
+    document.body.style.setProperty("--bg-color", "#444");
+    document.body.style.setProperty("--secondary-color", "#333");
+    document.body.style.setProperty("--text-color", "#fff");
 }

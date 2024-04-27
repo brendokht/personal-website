1d970ad3ed8e2cb1931663fe691f9fd21ead9ff4
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2024-04-26 20:38:40 -0400

    Added resume link

--- js/index.js
+++ js/index.js
@@ -1,42 +1,16 @@
+// Variables
 const navItems = document.getElementById("navLinks");
 const navLinks = document.getElementsByClassName("nav-link-item");
 const menuBtn = document.getElementById("menuButton");
 const closeBtn = document.getElementById("closeButton");
 const lightBtn = document.getElementById("lightMode");
 const darkBtn = document.getElementById("darkMode");
-
 let theme = "dark";
 
-navItems.addEventListener("transitionstart", function (event) {
-    if (event.propertyName === "top") {
-        closeBtn.classList.toggle("hide");
-        menuBtn.classList.toggle("hide");
-    }
-});
-
-navItems.addEventListener("transitionend", function (event) {
-    if (event.propertyName === "top") {
-        if (navItems.classList.contains("hidden")) {
-            navItems.style.zIndex = -1;
-        }
-        navItems.style.zIndex = 0;
-    }
-});
-
-navItems.addEventListener("transitionstart", function (event) {
-    navItems.style.zIndex = -1;
-});
-
-[].forEach.call(navLinks, function (navLink) {
-    navLink.addEventListener("click", function (event) {
-        navItems.classList.toggle("show");
-        navItems.classList.toggle("hidden");
-    });
-});
-
+// Functions
 function toggleMobileMenu() {
     navItems.classList.toggle("show");
-    navItems.classList.toggle("hidden");
+    navItems.classList.toggle("hide");
 }
 
 function changeTheme() {
@@ -57,9 +31,47 @@ function changeTheme() {
     }
 }
 
-if (window.innerWidth <= 768) {
-    closeBtn.classList.add("hide");
+function changeScreenSize() {
+    console.log("changeScreenSize");
+    if (window.innerWidth <= 768) {
+        navItems.classList.add("hide");
+        navItems.classList.remove("show");
+        closeBtn.classList.add("hide");
+        menuBtn.classList.remove("hide");
+    } else {
+        navItems.style.zIndex = 0;
+        closeBtn.classList.add("hide");
+        menuBtn.classList.remove("hide");
+        navItems.classList.remove("hide");
+    }
 }
+
+// Event Listeners and things to run
+navItems.addEventListener("transitionstart", function (event) {
+    if (event.propertyName === "top") {
+        closeBtn.classList.toggle("hide");
+        menuBtn.classList.toggle("hide");
+    }
+});
+
+window.addEventListener("resize", changeScreenSize);
+
+navItems.addEventListener("transitionend", function (event) {
+    if (event.propertyName === "top") {
+        if (navItems.classList.contains("hide")) {
+            navItems.style.zIndex = -1;
+        }
+        navItems.style.zIndex = 0;
+    }
+});
+
+navItems.addEventListener("transitionstart", function (event) {
+    navItems.style.zIndex = -1;
+});
+
+[].forEach.call(navLinks, function (navLink) {
+    navLink.addEventListener("click", toggleMobileMenu);
+});
 
 if (theme === "dark") {
     document.body.style.setProperty("--bg-color", "#444");

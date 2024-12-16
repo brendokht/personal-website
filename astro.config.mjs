106be9efb4941f03be287ae02a32158664d90355
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2024-12-16 04:07:27 -0500

    Added Astro Netlify integration package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: true,
-    },
-    integrations: [tailwind(), mdx()],
+  devToolbar: {
+      enabled: true,
+  },
+
+  integrations: [tailwind(), mdx()],
+  adapter: netlify(),
 });
